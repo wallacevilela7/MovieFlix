bcba50670d47e0d44c78fc68d525a22e6c37d58e
wallacevilela7 <wallace.vilela9@gmail.com>
2025-03-10 17:16:39 -0300

    feat: Create spring ap

---
+++ src/main/java/tech/wvs/movieflix/MovieFlixApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.movieflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFlixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieFlixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/wvs/movieflix/MovieFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.wvs.movieflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFlixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
